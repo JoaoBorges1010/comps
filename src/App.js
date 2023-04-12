2bf2b6ac65c269e1506d48fdba316e767338a775
joaoborges <joaoborgesbiz@gmail.com>
2023-04-12 12:11:57 +0100

    Created a reusable table and a sortable table component and a reusable sortable hook

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import AccordionPage from './pages/AccordionPage';
 import DropdownPage from "./pages/DropdownPage";
 import ButtonPage from "./pages/ButtonPage";
 import ModalPage from "./pages/ModalPage";
+import TablePage from "./pages/TablePage";
+import CounterPage from "./pages/CounterPage";
 
 function App() {
   return (
@@ -21,6 +23,12 @@ function App() {
         </Route>
         <Route path='/modal'>
           <ModalPage />
+        </Route>
+        <Route path='/table'>
+          <TablePage />
+        </Route>
+        <Route path='/counter'>
+          <CounterPage initialCount={10} />
         </Route>
       </div>
   </div>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,7 +5,9 @@ function Sidebar() {
     { label: 'Dropdown', path: '/'},
     { label: 'Accordion', path: '/accordion'},
     { label: 'Buttons', path: '/buttons'},
-    { label: 'Modal', path: '/modal'}
+    { label: 'Modal', path: '/modal'},
+    { label: 'Table', path: '/table'},
+    { label: 'Counter', path: '/counter'}
   ];
 
   const renderedLinks = links.map((link) => {
